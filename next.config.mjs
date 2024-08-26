1fa0e46eed568cb0f453e35f8fd25e162dad6cff
aditya955569 <aditya.kandpal2016@gmail.com>
2024-08-26 18:38:10 +0530

    bugs fixed

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const nextConfig = {
       return [
         {
           source: '/api/:path*',
-          destination: process.env.NEXT_PUBLIC_API_URL + '/:path*',
+          destination: process.env.NODE_ENV === 'production'
+            ? process.env.NEXT_PUBLIC_API_URL + '/:path*'
+            : 'http://localhost:5000/api/:path*',
         },
       ];
     },
